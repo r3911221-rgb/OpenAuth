6f6e0f224ee8552c8e6609811e41d07ec5acf864
r3911221-rgb <r3911221@gmail.com>
2026-06-30 21:49:54 +0530

    Create mobile/src/main/java/io/openauth/authenticator/core/database/security/DatabaseHealthChecker.kt

---
+++ mobile/src/main/java/io/openauth/authenticator/core/database/security/DatabaseHealthChecker.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2026 OpenAuth Authenticator
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.openauth.authenticator.core.database.security
+
+import io.openauth.authenticator.core.database.AppDatabase
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class DatabaseHealthChecker @Inject constructor(
+    private val database: AppDatabase
+) {
+    suspend fun isHealthy(): Boolean {
+        return try {
+            database.openHelper
+                .writableDatabase
+                .query("SELECT 1")
+                .close()
+            true
+        } catch (_: Exception) {
+            false
+        }
+    }
+}
