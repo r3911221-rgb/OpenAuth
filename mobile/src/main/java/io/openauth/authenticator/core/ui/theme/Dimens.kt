6f5587566703db7b07cf4bf9ff0a754f97cc8abd
r3911221-rgb <r3911221@gmail.com>
2026-07-01 17:15:24 +0530

    Create mobile/src/main/java/io/openauth/authenticator/core/ui/theme/Dimens.kt

---
+++ mobile/src/main/java/io/openauth/authenticator/core/ui/theme/Dimens.kt
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2026 OpenAuth Authenticator
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.openauth.authenticator.core.ui.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val SpacingXxs = 2.dp
+    val SpacingXs = 4.dp
+    val SpacingS = 8.dp
+    val SpacingM = 12.dp
+    val SpacingL = 16.dp
+    val SpacingXl = 24.dp
+    val SpacingXxl = 32.dp
+    val Spacing3xl = 48.dp
+    val Spacing4xl = 64.dp
+
+    val ScreenPaddingH = 16.dp
+    val ScreenPaddingV = 16.dp
+
+    val IconSizeS = 18.dp
+    val IconSizeM = 24.dp
+    val IconSizeL = 32.dp
+    val IconSizeXl = 48.dp
+
+    val AvatarSizeS = 36.dp
+    val AvatarSizeM = 44.dp
+    val AvatarSizeL = 56.dp
+
+    val ButtonHeight = 52.dp
+    val ButtonHeightS = 40.dp
+
+    val InputHeight = 56.dp
+    val ListItemHeight = 72.dp
+
+    val CountdownRingSize = 32.dp
+    val CountdownRingStroke = 3.dp
+
+    val BottomBarHeight = 80.dp
+    val TopBarHeight = 64.dp
+
+    val CardElevation = 2.dp
+    val DividerThickness = 0.5.dp
+}
