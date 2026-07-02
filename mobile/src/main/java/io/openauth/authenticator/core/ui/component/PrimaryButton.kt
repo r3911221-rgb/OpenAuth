00874fcdc033624e9550f7a12be5af4735c08c5e
r3911221-rgb <r3911221@gmail.com>
2026-07-02 08:01:27 +0530

    Create mobile/src/main/java/io/openauth/authenticator/core/ui/component/PrimaryButton.kt

---
+++ mobile/src/main/java/io/openauth/authenticator/core/ui/component/PrimaryButton.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2026 OpenAuth Authenticator
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.openauth.authenticator.core.ui.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import io.openauth.authenticator.core.ui.theme.Dimens
+
+@Composable
+fun PrimaryButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier.fillMaxWidth().height(Dimens.ButtonHeight),
+        enabled = enabled,
+        colors = ButtonDefaults.buttonColors(),
+        shape = androidx.compose.material3.MaterialTheme.shapes.medium,
+    ) {
+        Text(text = text)
+    }
+}
