f82eea414bf94eb6b5320524539d623a598b4e10
r3911221-rgb <r3911221@gmail.com>
2026-07-02 08:16:46 +0530

    Create mobile/src/main/java/io/openauth/authenticator/core/ui/icon/AppIcons.kt

---
+++ mobile/src/main/java/io/openauth/authenticator/core/ui/icon/AppIcons.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2026 OpenAuth Authenticator
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.openauth.authenticator.core.ui.icon
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.*
+import androidx.compose.ui.graphics.vector.ImageVector
+
+object AppIcons {
+    val Add = Icons.Filled.Add
+    val Settings = Icons.Filled.Settings
+    val Search = Icons.Filled.Search
+    val Close = Icons.Filled.Close
+    val Delete = Icons.Filled.Delete
+    val Edit = Icons.Filled.Edit
+    val Copy = Icons.Filled.ContentCopy
+    val Share = Icons.Filled.Share
+    val Backup = Icons.Filled.Backup
+    val Restore = Icons.Filled.Restore
+    val Lock = Icons.Filled.Lock
+    val Unlock = Icons.Filled.LockOpen
+    val Fingerprint = Icons.Filled.Fingerprint
+    val Visibility = Icons.Filled.Visibility
+    val VisibilityOff = Icons.Filled.VisibilityOff
+    val DarkMode = Icons.Filled.DarkMode
+    val LightMode = Icons.Filled.LightMode
+    val Info = Icons.Filled.Info
+    val Warning = Icons.Filled.Warning
+    val Error = Icons.Filled.Error
+    val Check = Icons.Filled.Check
+    val ArrowBack = Icons.Filled.ArrowBack
+    val More = Icons.Filled.MoreVert
+    val Refresh = Icons.Filled.Refresh
+    val QrCode = Icons.Filled.QrCode
+    val Group = Icons.Filled.Folder
+    val Tag = Icons.Filled.Label
+    val Star = Icons.Filled.Star
+    val StarOutline = Icons.Outlined.StarBorder
+    val Timer = Icons.Filled.Timer
+    val History = Icons.Filled.History
+    val Empty = Icons.Outlined.Inbox
+    val Sort = Icons.Filled.Sort
+    val Filter = Icons.Filled.FilterList
+    val Palette = Icons.Filled.Palette
+    val Grid = Icons.Filled.GridView
+    val List = Icons.Filled.ViewList
+    val Download = Icons.Filled.Download
+    val Upload = Icons.Filled.Upload
+    val Cloud = Icons.Filled.Cloud
+    val Shield = Icons.Filled.Shield
+    val Bug = Icons.Filled.BugReport
+    val Help = Icons.Filled.Help
+    val Mail = Icons.Filled.Email
+    val Link = Icons.Filled.Link
+    val Person = Icons.Filled.Person
+    val People = Icons.Filled.People
+    val Notifications = Icons.Filled.Notifications
+    val Dashboard = Icons.Filled.Dashboard
+    val Exit = Icons.Filled.ExitToApp
+}
