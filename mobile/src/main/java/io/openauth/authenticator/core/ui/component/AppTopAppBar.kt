4b2caa61c2f9214899dd473ec75880ce1cc1fb6f
r3911221-rgb <r3911221@gmail.com>
2026-07-02 08:02:44 +0530

    Create mobile/src/main/java/io/openauth/authenticator/core/ui/component/AppTopAppBar.kt

---
+++ mobile/src/main/java/io/openauth/authenticator/core/ui/component/AppTopAppBar.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2026 OpenAuth Authenticator
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.openauth.authenticator.core.ui.component
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AppTopAppBar(
+    title: String,
+    navigationIcon: @Composable () -> Unit = {},
+    actions: @Composable () -> Unit = {},
+) {
+    TopAppBar(
+        title = { Text(text = title) },
+        navigationIcon = navigationIcon,
+        actions = { actions() },
+    )
+}
